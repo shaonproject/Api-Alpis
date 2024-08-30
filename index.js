8e387826f2cefd9acbe81f600f04723d945a8477
shaonproject <153928138+shaonproject@users.noreply.github.com>
2024-08-30 16:37:27 +0600

    Update and rename server.js to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("./settings");
 const http = require("http");
-const app = require("./index");
+const app = require("./server");
 const PORTHOST = port || 8080;
 
 http.createServer(app).listen(PORTHOST, () => {
